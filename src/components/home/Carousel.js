94ce5455c677118388bc09638f644201e401e60c
Jacobokia <cobs399@gmail.com>
2020-08-15 17:44:18 +0300

    adds the pages folders and component folders

---
+++ src/components/home/Carousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Carousel() {
+  return (
+    <div>
+      <p>Carousel</p>
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "../components/home/Carousel";
+import Cards from '../components/home/Cards'
+
+export default function Home() {
+  return (
+    <div>
+      <Carousel />
+      <Cards/>
+    </div>
+  );
+}
