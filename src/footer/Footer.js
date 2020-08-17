b10913af2dd0b152d7024fb2aee760974ec2dc6f
Jacobokia <cobs399@gmail.com>
2020-08-17 08:06:13 +0300

    creates a footer and adds it to the app folder

---
+++ src/footer/Footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <h4>Contact us</h4>
+            <ul className="list-unstyled">
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+            </ul>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <h4>Contact us</h4>
+            <ul className="list-unstyled">
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Contact us</h4>
+            <ul className="list-unstyled">
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Contact us</h4>
+            <ul className="list-unstyled">
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+              <li>lorem ipsum</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+            <p className="text-xs-center">
+                &copy;{new Date().getFullYear()} Mushi Concepts. All rights reserved.
+            </p>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
