b4c6439c313c0a1a78bd1f544909fbf5bea13fcb
Jacobokia <cobs399@gmail.com>
2020-08-19 06:20:55 +0300

    add the social media icons and fix the footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div className="main-footer">
       <div className="container">
         <div className="row">
-          <div className="col-md-3 col-sm-6">
+          <div className="col-md-3 col-sm-6 col-xs-12">
             <h4>Contact us</h4>
             <ul className="list-unstyled">
               <li>lorem ipsum</li>
@@ -15,7 +15,7 @@ function Footer() {
             </ul>
           </div>
 
-          <div className="col-md-3 col-sm-6">
+          <div className="col-md-3 col-sm-6 col-xs-12">
             <h4>Contact us</h4>
             <ul className="list-unstyled">
               <li>lorem ipsum</li>
@@ -24,7 +24,7 @@ function Footer() {
               <li>lorem ipsum</li>
             </ul>
           </div>
-          <div className="col-md-3 col-sm-6">
+          <div className="col-md-3 col-sm-6 col-xs-12">
             <h4>Contact us</h4>
             <ul className="list-unstyled">
               <li>lorem ipsum</li>
@@ -33,14 +33,15 @@ function Footer() {
               <li>lorem ipsum</li>
             </ul>
           </div>
-          <div className="col-md-3 col-sm-6">
-            <h4>Contact us</h4>
-            <ul className="list-unstyled">
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-            </ul>
+          <div className="col-md-3 col-sm-6 col-xs-12">
+            <h2> Follow us</h2>
+            <p>Please follow us on our Social Media Profile to keep updated.</p>
+           
+              <a href="https://www.facebook.com/cobs.okia" ><i class="fa fa-facebook"></i></a>
+             <a href="https://www.facebook.com/cobs.okia" ><i class="fa fa-twitter"></i></a>
+              <a href="https://www.facebook.com/cobs.okia"><i class="fa fa-instagram" ></i></a>
+              <a href="https://www.linkedin.com/in/jacob-okia-1ba0b8117/"><i class="fa fa-linkedin"></i>linkedin</a>
+           
           </div>
         </div>
 
