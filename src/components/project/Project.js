ff264dcdcd73f3dfe53c20a93fe3bda9e2cb0d9c
KennyKennethOcitti <kennethocitti95@gmail.com>
2020-08-19 17:34:12 +0300

    Css

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -4,7 +4,7 @@ export default class Project extends Component {
   render() {
     return (
       <div className="homeDiv">
-        <div className="projectDiv">
+        <div className="projects">
           <p style={{ color: "yellow" }}>Some Texts Here Jal</p>
           <p style={{ color: "yellow" }}>
             THIS IS WHERE OUR TEXT HEADING WILL BE JAL
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,30 +1,5 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
-const Footer = () => {
-  return (
-    <div style={{ backgroundColor: "#D0D3D4", height: "40%" }}>
-      <MDBFooter className="font-small pt-4 mt-4 ">
-        <MDBContainer fluid className="text-center text-md-left">
-          <MDBRow>
-            <MDBCol md="6">
-              <h5 className="title">About Bytehiveguru</h5>
-              <p>Bytehiveguru content here, contact, phone</p>
-            </MDBCol>
-            <MDBCol md="6">
-              <h5 className="title">Our contacts and location</h5>
-              <p>Plot 3309 Koro road. Located opposite old Kabaka's palace</p>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-        <div className="footer-copyright text-center py-3">
-          <MDBContainer fluid>
-            &copy; {new Date().getFullYear()} Copyright: Iformbyte.com
-          </MDBContainer>
-        </div>
-      </MDBFooter>
-    </div>
-  );
-};
-
-export default Footer;
+export default function Footer() {
+  return <div>dgdggdggd</div>;
+}
