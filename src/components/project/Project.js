23ccdb29ac108d088b421d992131d62580136236
KennyKennethOcitti <kennethocitti95@gmail.com>
2020-08-19 16:13:35 +0300

    small changes

---
+++ src/components/project/Project.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class Project extends Component {
+  render() {
+    return (
+      <div className="homeDiv">
+        <div className="projectDiv">
+          <p style={{ color: "yellow" }}>Some Texts Here Jal</p>
+          <p style={{ color: "yellow" }}>
+            THIS IS WHERE OUR TEXT HEADING WILL BE JAL
+          </p>
+        </div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+        <div>Cards here</div>
+      </div>
+    );
+  }
+}
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,57 +1,30 @@
 import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3 col-sm-6">
-            <h4>Contact us</h4>
-            <ul className="list-unstyled">
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-            </ul>
-          </div>
-
-          <div className="col-md-3 col-sm-6">
-            <h4>Contact us</h4>
-            <ul className="list-unstyled">
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-            </ul>
-          </div>
-          <div className="col-md-3 col-sm-6">
-            <h4>Contact us</h4>
-            <ul className="list-unstyled">
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-            </ul>
-          </div>
-          <div className="col-md-3 col-sm-6">
-            <h4>Contact us</h4>
-            <ul className="list-unstyled">
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-            </ul>
-          </div>
+    <div style={{ backgroundColor: "#D0D3D4", height: "40%" }}>
+      <MDBFooter className="font-small pt-4 mt-4 ">
+        <MDBContainer fluid className="text-center text-md-left">
+          <MDBRow>
+            <MDBCol md="6">
+              <h5 className="title">About Bytehiveguru</h5>
+              <p>Bytehiveguru content here, contact, phone</p>
+            </MDBCol>
+            <MDBCol md="6">
+              <h5 className="title">Our contacts and location</h5>
+              <p>Plot 3309 Koro road. Located opposite old Kabaka's palace</p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+        <div className="footer-copyright text-center py-3">
+          <MDBContainer fluid>
+            &copy; {new Date().getFullYear()} Copyright: Iformbyte.com
+          </MDBContainer>
         </div>
-
-        <div className="footer-bottom">
-            <p className="text-xs-center">
-                &copy;{new Date().getFullYear()} Mushi Concepts. All rights reserved.
-            </p>
-
-        </div>
-      </div>
+      </MDBFooter>
     </div>
   );
-}
+};
+
 export default Footer;
