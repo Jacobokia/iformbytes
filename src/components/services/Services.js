31e262062391f83388b6768de3666bece1db7f9e
KennyKennethOcitti <kennethocitti95@gmail.com>
2020-08-20 20:40:51 +0300

    Screen responsiveness

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -4,40 +4,49 @@ import { Card } from "react-bootstrap";
 export default function Services() {
   const cardInfo = [
     {
-      image: "https://picsum.photos/1280/520",
-      title: "Title 1",
-      text: "text for image 1",
+      // image: "https://picsum.photos/1280/520",
+      title: "01",
+      text:
+        "I enjoy taking on challenges and engaging with the resources around me and the provision of solutions and sharing with my teammates",
     },
     {
-      image: "https://picsum.photos/1280/510",
-      title: "title 2",
-      text: "text for image 2",
+      // image: "https://picsum.photos/1280/510",
+      title: "02",
+      text:
+        "I enjoy taking on challenges and engaging with the resources around me and the provision of solutions and sharing with my teammates",
     },
     {
-      image: "https://picsum.photos/1280/520",
-      title: "title 3",
-      text: "text for image 3",
+      // image: "https://picsum.photos/1280/520",
+      title: "03",
+      text:
+        "I enjoy taking on challenges and engaging with the resources around me and the provision of solutions and sharing with my teammates",
     },
     {
-      image: "https://picsum.photos/1280/520",
-      title: " title 4",
-      text: "text for image 4",
+      // image: "https://picsum.photos/1280/520",
+      title: "04",
+      text:
+        "I enjoy taking on challenges and engaging with the resources around me and the provision of solutions and sharing with my teammates",
     },
   ];
 
   const renderCard = (card, index) => {
     return (
-      <Card style={{ width: "18rem" }} key={index} className="box">
-        <Card.Img style={{ width: "18rem" }} variant="top" src={card.image} />
+      <Card
+        style={{ width: "18rem", marginTop: "7%" }}
+        key={index}
+        className="box"
+      >
+        {/* <Card.Img style={{ width: "18rem" }} variant="top" src={card.image} /> */}
         <Card.Body>
-          <Card.Title>{card.title}</Card.Title>
-          <Card.Text>{card.text}</Card.Text>
+          <p style={{ textAlign: "center" }}>Why Choose Us</p>
+          <Card.Title style={{ textAlign: "center" }}>{card.title}</Card.Title>
+          <Card.Text style={{ textAlign: "center" }}>{card.text}</Card.Text>
         </Card.Body>
       </Card>
     );
   };
   return (
-    <div className="">
+    <div style={{ flexDirection:"row"}}>
       <div className="service">
         <p>Services</p>
         <p>We can serve you better</p>
